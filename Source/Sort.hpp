005a30cd7d78737a3e3d6fbdd986748b0f1e3bb0
wsl20 <wsl20@ic.ac.uk>
2023-02-27 14:51:27 +0000

    feat: modified quicksort to be able to sort on either the first or second element in the pair first

--- Source/Sort.hpp
+++ Source/Sort.hpp
@@ -7,26 +7,36 @@ using namespace std;
 // Sort either on first elem or second elem in the pair; set using elem
 
 // returns 1 if a > b, 0 if a == b and -1 if a < b
-int compare_pair(std::pair<int64_t, int64_t> a, std::pair<int64_t, int64_t> b,){
-    if (a.first == b.first){
-        if (a.second > b.second){
+int compare_pair(std::pair<int64_t, int64_t> a, std::pair<int64_t, int64_t> b, int on_elem){
+    int a_f = a.first, a_s = a.second;
+    int b_f = b.first, b_s = b.second;
+    
+    if(on_elem) {
+        // Compare on second element in pair first
+        if((a_s > b_s) || (a_s == b_s && a_f > b_f)){
             return 1;
-        } else if (a.second < b.second){
+        } else if ((a_s < b_s) || (a_s == b_s && a_f < b_f)){
             return -1;
-        } else{
+        } else {
             return 0;
         }
-    } else if (a.first > b.first){
-        return 1;
+    } else{
+        // Compare on first element in pair first
+        if((a_f > b_f) || (a_f == b_f && a_s > b_s)){
+            return 1;
+        } else if ((a_f < b_f) || (a_f == b_f && a_s < b_s)){
+            return -1;
+        } else {
+            return 0;
+        }
     }
-    return -1;
 }
 
-int partition(std::vector<std::pair<int64_t, int64_t>>& v, int low, int high){
+int partition(std::vector<std::pair<int64_t, int64_t>>& v, int low, int high, int on_elem){
     std::pair<int64_t, int64_t> pivot = v[high];
     int i = low-1;
     for (int j = low; j < high; j++) {
-        if (compare_pair(pivot, v[j]) != -1) {
+        if (compare_pair(pivot, v[j], on_elem) != -1) {
              
             // elem smaller than pivot, so swap
             i++;
@@ -42,12 +52,12 @@ int partition(std::vector<std::pair<int64_t, int64_t>>& v, int low, int high){
     return (i + 1);
 }
 
-void quickSort(std::vector<std::pair<int64_t, int64_t>>& v, int low, int high) {
+void quickSort(std::vector<std::pair<int64_t, int64_t>>& v, int low, int high, int on_elem) {
     if (low < high) {
-        int p = partition(v, low, high);
+        int p = partition(v, low, high, on_elem);
         // Recurse on the left of pivot
-        quickSort(v, low, p - 1);
+        quickSort(v, low, p - 1, on_elem);
         // Recurse on the right of pivot
-        quickSort(v, p + 1, high);
+        quickSort(v, p + 1, high, on_elem);
     }
 }
